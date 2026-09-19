01feca1cf5e4828aec8b12477edcda04c836c460
yanagihara-inc <info@yanagihara.jp>
2026-09-19 16:41:41 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'rakuraku-kintai-v14';
+const CACHE = 'rakuraku-kintai-v15';
 const APP_FILES = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', event => {
